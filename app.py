dff029fdbae4cd2a4bcbc4783b4a1f8e95945756
mriusero <160402415+mriusero@users.noreply.github.com>
2025-06-06 11:51:57 +0200

    feat: 1st draw ui

--- app.py
+++ app.py
@@ -1,64 +1,56 @@
 import gradio as gr
-from huggingface_hub import InferenceClient
 
-"""
-For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
-"""
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+from src.chat import respond
 
-
-def respond(
-    message,
-    history: list[tuple[str, str]],
-    system_message,
-    max_tokens,
-    temperature,
-    top_p,
-):
-    messages = [{"role": "system", "content": system_message}]
-
-    for val in history:
-        if val[0]:
-            messages.append({"role": "user", "content": val[0]})
-        if val[1]:
-            messages.append({"role": "assistant", "content": val[1]})
-
-    messages.append({"role": "user", "content": message})
-
-    response = ""
-
-    for message in client.chat_completion(
-        messages,
-        max_tokens=max_tokens,
-        stream=True,
-        temperature=temperature,
-        top_p=top_p,
-    ):
-        token = message.choices[0].delta.content
-
-        response += token
-        yield response
-
-
-"""
-For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
-"""
-demo = gr.ChatInterface(
-    respond,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
+custom_theme = gr.themes.Base(
+    primary_hue="blue",
+    secondary_hue="green",
+    neutral_hue="gray",
+    font=[gr.themes.GoogleFont("Open Sans"), "sans-serif"],
 )
 
+with gr.Blocks(theme=custom_theme) as demo:
+
+    gr.Markdown("# IndustryMind AI ⚡️")
+    gr.Markdown("## An industrial production intelligence (WIP)")
+
+    with gr.Tab("Demo"):
+        gr.Markdown(
+            """
+            This demo showcases the capabilities of IndustryMind AI.
+            You can interact with the chatbot to get insights and assistance on production-related queries.
+            """
+        )
+
+        with gr.Row():
+            with gr.Column(scale=1):
+                chatbot = gr.ChatInterface(respond)
+                with gr.Row():
+                    with gr.Column(scale=2):
+                        with gr.Group():
+                            button1 = gr.Button("Start Production")
+                            button2 = gr.Button("Restart Production")
+                            button3 = gr.Button("Stop Production")
+
+                            output1 = gr.Textbox(label="Sortie", visible=False)
+                            output2 = gr.Textbox(label="Sortie", visible=False)
+                            output3 = gr.Textbox(label="Sortie", visible=False)
+
+                            button1.click(fn=None, outputs=output1)
+                            button2.click(fn=None, outputs=output2)
+                            button3.click(fn=None, outputs=output3)
+
+            with gr.Column(scale=2):
+                with gr.Group():
+                    gr.Markdown('### Display Graphs here')
+
+    with gr.Tab("Description"):
+        gr.Markdown(
+            """
+            IndustryMind AI is an AI-powered chatbot designed to assist with industrial production processes. 
+            It can help you manage production lines, monitor equipment, and optimize workflows.
+            """
+        )
 
 if __name__ == "__main__":
     demo.launch()
